bb5611ea0ed365566c63bf55986b82198594c768
Sabbha20 <sabbha.power1@gmail.com>
2024-08-29 13:40:05 +0530

    UI created for the app

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from PIL import Image
+import secrets
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+# generate random secret key
+app.config['SECRET_KEY'] = secrets.token_hex(24)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+@app.route('/')
+def home():
+    # home page
+    
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["/static/images/placeholder_image.png" for i in range(3)]
+    )
+
+@app.route('/prompt', methods=['POST', 'GET'])
+def prompt():
+    # generate images from user prompt
+    print("user prompt received:", request.form['prompt_input'])
+    
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["/static/images/placeholder_image.png" for i in range(3)]
+    )
+
+@app.route('/supersample', methods=['POST', 'GET'])
+def supersample():
+    # enlarge and save prompt image in high quality
+    print("save button", request.form['save_btn'], "was clicked!")
+
+    return render_template(
+        "index.html", 
+        # pass variables into the HTML template
+        btn_range = range(3), 
+        prompt_images = ["/static/images/placeholder_image.png" for i in range(3)]
+    )
+
+if __name__ == '__main__':
+    # run application
+    app.run(
+        host = '0.0.0.0', 
+        port = 8000, 
+        debug = True
+    )   
